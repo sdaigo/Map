95e47323efec0bf52f2151635e661b15937d6a15
Daigo Shitara <stain.witness+github@gmail.com>
2021-06-19 14:33:16 +0900

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by shitara on 2021/06/19.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
